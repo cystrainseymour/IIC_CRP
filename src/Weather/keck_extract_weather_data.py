4e3752e08e0ed6b1e40ca3e803003ea6b575d5be
Cy Strain-Seymour <cystrainseymour@gmail.com>
2025-07-01 16:01:19 -0400

    keck_extract_weather_data.py now accepts file with times and extracts only weather data entries for those times

--- src/Weather/keck_extract_weather_data.py
+++ src/Weather/keck_extract_weather_data.py
@@ -1,10 +1,17 @@
 import sys
+import os
 
 def is_before(date1, date2):
     for i in range(5):
         if date1[i] < date2[i]:
             return True
     return False
+
+def is_equal(date1, date2):
+    for i in range(5):
+        if date1[i] != date2[i]:
+            return False
+    return True
     
 def extract_date(d, t):
     date = list(map(int,d.split("/")))
@@ -61,34 +68,100 @@ def find_relevant_rows(inp, begin, end, entries):
     
     return rows
     
+def extract_date_str(date_str):
+    begin_str = date_str.strip().split(" ")
+    time = begin_str[0].split(":")
+    
+    time = list(map(int,begin_str[0].split(":")))
+    
+    if begin_str[1].lower().startswith("p") and time[0] != 12:
+        time[0] += 12
+    elif not begin_str[1].lower().startswith("p") and time[0] == 12:
+        time[0] = 0
+    
+    calendar = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    
+    date = [25] + [i+1 for i in range(len(calendar)) if calendar[i] == begin_str[2]] + [int(begin_str[3])]
+    
+    return date + time
+    
+def find_relevant_rows_given_times(dat_inp, tim_inp, entries):
+    line = dat_inp.readline().split("\t")
+    
+    date = extract_date(line[0], line[1])
+    
+    tim_inp.readline()
+    begin = extract_date_str(tim_inp.readline())
+    
+    while is_before(date, begin):
+        line = dat_inp.readline().split("\t")
+        try:
+            date = extract_date(line[0], line[1])
+        except IndexError:
+            print("Nothing found")
+            return []
+    
+    rows = []
+    rows.append(find_relevant_entries(line, entries))
+    cur = extract_date_str(tim_inp.readline())
+    
+    line = dat_inp.readline().split("\t")
+    date = extract_date(line[0], line[1])
+    print(date, cur)
+    
+    while True:
+        try:
+            if is_equal(cur, date):
+                rows.append(find_relevant_entries(line, entries))
+                cur = extract_date_str(tim_inp.readline())
+            else:    
+                line = dat_inp.readline().split("\t")
+                date = extract_date(line[0], line[1])
+                
+            print(date, cur)
+        except Exception as e:
+            print(e)
+            break
+    
+    return rows
+    
 def main():
     args = sys.argv[1:]
-    if len(args) < 9:
-        args = input("Incorrect number of arguments. Please try again: ").split()
+    if len(args) < 3:
+        args = input("Incorrect number of arguments. Please try again").split()
+        sys.exit()
     inp = open(args[0], "r")
     
     mode = "w"
-    
-    begin = extract_date(args[2], " ".join([args[3], args[4]]))
-    end = extract_date(args[5], " ".join([args[6], args[7]]))
+    tim_inp = None
+    start = 8
+    if os.path.exists(args[2]):
+        tim_inp = open(args[2],"r")
+        start = 3
+    else:
+        begin = extract_date(args[2], " ".join([args[3], args[4]]))
+        end = extract_date(args[5], " ".join([args[6], args[7]]))
     #print(begin, end)
     
     vars = extract_col_names(inp.readline(), inp.readline())
     #print(vars)
     entries = [False] * len(vars)
     for i in range(len(vars)):
-        for j in range(8, len(args)):
-            if j == len(args)-1 and args[j] == "a":
+        for j in range(start, len(args)):
+            if j == len(args)-1 and args[j].lower() == "a":
                 mode = "a"
                 break
-            if vars[i].startswith(args[j]):
+            if vars[i].lower().startswith(args[j].lower()):
                 entries[i] = True
                 break
     
     entries[0] = entries[1] = True # date and time always included
     #print(entries)
-    
-    rel_rows = find_relevant_rows(inp, begin, end, entries)
+    rel_rows = []
+    if tim_inp is None:
+        rel_rows = find_relevant_rows(inp, begin, end, entries)
+    else:
+        rel_rows = find_relevant_rows_given_times(inp, tim_inp, entries)
     
     with open(args[1], mode) as outp:
         for i in range(len(entries)):
