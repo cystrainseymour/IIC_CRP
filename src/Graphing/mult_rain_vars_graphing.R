414b0bc5d05d06b73c0bf27ab6836e83d7fdf9d7
Cy Strain-Seymour <cystrainseymour@gmail.com>
2025-07-01 14:57:18 -0400

    better graphing for rain variables

--- src/Graphing/mult_rain_vars_graphing.R
+++ src/Graphing/mult_rain_vars_graphing.R
@@ -5,31 +5,29 @@ graph <- function(path){
 	fig <- plot_ly(width = 1500)
 
 	turb_color <- "#ff7f0e"
-	hourly_rain_color <- "#1f77b4"
-	hourly_rain_1hr_color <- "#08706c"
-	hourly_rain_2hr_color <- "#016524"
-	hr24_rain_color <- "#9467bd"
+	rate_color <- "#ffff00"
+	rain_color <- "#1f77b4"
+	rain_prev_color <- "#08706c"
+	rain_2prev_color <- "#016524"
+	rain_24_color <- "#9467bd"
 	
-	colors <- c(turb_color, hourly_rain_color, hourly_rain_1hr_color, hourly_rain_2hr_color, hr24_rain_color)
+	colors <- c(turb_color, rate_color, rain_color, rain_prev_color, rain_2prev_color, rain_24_color)
 
 	rain <- list(
-	  tickfont = list(color = colors[2]),
-	  titlefont = list(color = colors[2]),
+	  tickfont = list(color = colors[3]),
+	  titlefont = list(color = colors[3]),
 	  ticksuffix = " in",
-	  tickvals = (1:20)/10,
+	  tickvals = (1:20)/2,
 	  overlaying = "y",
 	  side = "right",
 	  showgrid = FALSE)
+	  
+	fig <- fig %>% add_trace(x = as.list(my_data[,1]), y = my_data[,2], mode = "lines+markers", name = colnames(my_data)[2], type = "scatter", marker = list(color = colors[1], showticklabels = FALSE), line = list(color = colors[1]))
 
-	fig <- fig %>% add_trace(x = as.list(my_data$Time), y = my_data$Average.Turbidity, mode = "lines+markers", name = "Average Turbidity", type = "scatter", marker = list(color = colors[1], showticklabels = FALSE), line = list(color = colors[1]))
-
-	fig <- fig %>% add_trace(x = as.list(my_data$Time), y = my_data$Hourly.Rainfall, yaxis = "y2", mode = "lines+markers", name = "Hourly Rainfall (in)", type = "scatter", marker = list(color = colors[2], showticklabels = FALSE), line = list(color = colors[2]))
-
-	fig <- fig %>% add_trace(x = as.list(my_data$Time), y = my_data$Hourly.Rainfall.1hr.ago, yaxis = "y2", mode = "lines+markers", name = "Hourly Rainfall 1hr ago (in)", type = "scatter", marker = list(color = colors[3], showticklabels = FALSE), line = list(color = colors[3]))
-
-	fig <- fig %>% add_trace(x = as.list(my_data$Time), y = my_data$Hourly.Rainfall.2hrs.ago, yaxis = "y2", mode = "lines+markers", name = "Hourly Rainfall 2hrs ago (in)", type = "scatter", marker = list(color = colors[4], showticklabels = FALSE), line = list(color = colors[4]))
-
-	fig <- fig %>% add_trace(x = as.list(my_data$Time), y = my_data$Rainfall.Past.24.Hours, yaxis = "y2", mode = "lines+markers", name = "Rainfall over past 24hrs (in)", type = "scatter", marker = list(color = colors[5], showticklabels = FALSE), line = list(color = colors[5]))
+	for(i in 3:length(colnames(my_data)))
+	{
+		fig <- fig %>% add_trace(x = as.list(my_data[,1]), y = my_data[,i], yaxis = "y2", mode = "lines+markers", name = colnames(my_data)[i], type = "scatter", marker = list(color = colors[i-1], showticklabels = FALSE), line = list(color = colors[i-1]))
+	}
 
 	fig <- fig %>% layout(
 	  title = "Chickahominy 6/6 - 6/7", 
